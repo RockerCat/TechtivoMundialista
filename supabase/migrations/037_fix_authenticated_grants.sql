d3805f7ed1f8a40affdf5adaa71bf5bd06f7b892
Alex Sosa <alexsosa.me@gmail.com>
2026-06-05 16:41:03 -0500

    Initial Techtivo Mundialista setup completed

---
+++ supabase/migrations/037_fix_authenticated_grants.sql
@@ -0,0 +1,4 @@
+grant select on public.group_members to authenticated;
+grant select on public.groups to authenticated;
+grant select on public.user_profiles to authenticated;
+grant select on public.admin_users to authenticated;
